ff538fabf3be2ca4158b265ceeb2e38eca74326e
Panteve <diegorojer@gmail.com>
2024-03-25 20:54:23 -0500

    empezando el backend

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+// Settings
+const app = express();
+app.set("port", process.env.PORT || 3000);
+
+// Middleware
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:4321" }));
+
+//Conexión BD Atlas
+mongoose.connect(process.env.MONGODB_URI, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+})
+  .then(() => console.log("Conectado a la BD"))
+  .catch((error) => console.error("Error de conexión a la BD:", error));
+
+// Rutas
+//app.use("/api", require("./routes/cliente.route"));
+
+// Middleware de manejo de errores
+app.use((err, res) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Error interno del servidor" });
+});
+
+// Iniciar el servidor
+app.listen(app.get("port"), () => {
+  console.log("Servidor activo en el puerto", app.get("port"));
+});
