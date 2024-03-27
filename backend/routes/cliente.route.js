6ee9ffecce4b58a1bc68f11e0318f0d0741faba1
Panteve <diegorojer@gmail.com>
2024-03-27 12:56:43 -0500

    un cambio

--- backend/routes/cliente.route.js
+++ backend/routes/cliente.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const clienteCtrl = require("../controllers/cliente.controller")
+
+const clienteCtrl = require("../controllers/cliente.controller");
 
 router.get("/cliente", clienteCtrl.getCliente);
 router.post("/cliente", clienteCtrl.createCliente);
