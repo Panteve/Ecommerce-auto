bb69aa78bb454b0e205fc18a8c4af2e011682f6f
Panteve <diegorojer@gmail.com>
2024-04-01 17:38:48 -0500

    Page of register created with routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/component/login.component';
+import { RegisterComponent } from './pages/register/component/register.component'
 export const routes: Routes = [
     {
         path: 'login',
-        title: 'Iniciar sesion',
+        title: 'Ecommerce-iniciar sesion',
         component: LoginComponent,
+    },
+    {
+        path: 'register',
+        title: 'Ecommerce-registrarse',
+        component: RegisterComponent,
     },
 ];
 
