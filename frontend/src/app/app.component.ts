a5706576156fe9909cc618495b6578b9682ab8d7
Panteve <diegorojer@gmail.com>
2024-04-02 16:54:47 -0500

    Footer hecho y pagina de inicio iniciada

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet} from '@angular/router';
-
-
+import { RouterLink, RouterOutlet, Router} from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, RouterLink],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, RouterLink]
 })
 export class AppComponent {
-  isloggedIn = true;
+  isloggedIn = false;
+
+
+  constructor(
+    private router: Router
+  ){}
+
+  isHomePage(): boolean {
+    return this.router.url === '/';
+  }
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/component/login.component';
 import { RegisterComponent } from './pages/register/component/register.component'
+
 export const routes: Routes = [
     {
         path: 'login',
