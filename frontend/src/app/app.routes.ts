af49c8094191f84bddb2df4901f6db9836275a6f
Panteve <diegorojer@gmail.com>
2024-04-03 13:07:38 -0500

    Creacion de componente para hompage

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/component/login.component';
 import { RegisterComponent } from './pages/register/component/register.component'
+import { HomepageComponent } from './pages/home/component/homepage.component';
 
 export const routes: Routes = [
+    {
+        path:'',
+        title: 'SportLife-home',
+        component: HomepageComponent
+
+    },
     {
         path: 'login',
         title: 'Ecommerce-iniciar sesion',
