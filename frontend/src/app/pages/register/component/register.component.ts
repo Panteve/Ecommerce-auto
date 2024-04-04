5902472d9289ea4c6d1d2c0fc2faa7ca40598ee8
Panteve <diegorojer@gmail.com>
2024-04-03 20:53:20 -0500

    Problemas de SEO,accsibilidad y rendiminetos arreglados

--- frontend/src/app/pages/register/component/register.component.ts
+++ frontend/src/app/pages/register/component/register.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NgOptimizedImage],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
