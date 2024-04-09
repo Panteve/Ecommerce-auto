b596a22ad11251c585e6c8d553428d82fbfff1a4
Panteve <diegorojer@gmail.com>
2024-04-09 13:36:00 -0500

    Pedido info component creado con su respectivo routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,30 +1,36 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './pages/home/component/homepage.component';
 import { LoginComponent } from './pages/login/component/login.component';
 import { RegisterComponent } from './pages/register/component/register.component'
-import { HomepageComponent } from './pages/home/component/homepage.component';
 import { ClienteInfoComponent } from './pages/cliente-info/component/cliente-info.component';
+import { PedidoInfoComponent } from './pages/pedido-info/component/pedido-info.component';
 
 export const routes: Routes = [
     {
         path:'',
-        title: 'SportLife-home',
+        title: 'SportLife-Home',
         component: HomepageComponent
 
     },
     {
         path: 'login',
-        title: 'SportLife-iniciar sesion',
+        title: 'SportLife-Iniciar sesion',
         component: LoginComponent,
     },
     {
         path: 'register',
-        title: 'SportLife-registrarse',
+        title: 'SportLife-Registrarse',
         component: RegisterComponent,
     },
     {
         path: 'client-info',
-        title: 'SportLife-cuenta',
+        title: 'SportLife-Cuenta',
         component: ClienteInfoComponent,
+    },
+    {
+        path: 'pedido/:refpedido',
+        title: 'SportLife-Informacion del pedido',
+        component: PedidoInfoComponent,
     },
 ];
 
---
+++ frontend/src/app/pages/pedido-info/component/pedido-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pedido-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './pedido-info.component.html',
+  styleUrl: './pedido-info.component.css'
+})
+export class PedidoInfoComponent {
+
+}
