77f1e2743dae7dfe67663dd8d21eea6012a6ad06
Panteve <diegorojer@gmail.com>
2024-04-12 20:51:25 -0500

    Suscrito al obsevacle de islogged

--- frontend/src/app/pages/pedido-info/component/pedido-info.component.ts
+++ frontend/src/app/pages/pedido-info/component/pedido-info.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PedidoService } from '../../../services/pedido.service';
 import { CurrencyPipe, DatePipe } from '@angular/common';
+import { ClienteService } from '../../../services/cliente.service';
 
 
 @Component({
@@ -16,16 +17,23 @@ export class PedidoInfoComponent {
   pedido: any;
   
   constructor(
+    private clienteService:ClienteService ,
+    private router: Router,
     private route: ActivatedRoute,
     public pedidoService: PedidoService
   ){}
   
+  ngAfterContentInit(): void {
+    if(this.clienteService.isLoggedIn){
+       this.router.navigate([''])
+    }
+   }
+
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       let id = params['refpedido'] 
       this.pedidoService.unicoPedido(id).subscribe(pedido => {
         this.pedido = pedido
-        console.log(pedido)
       });
     });
   }
