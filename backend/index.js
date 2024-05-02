21a11f5c9536c90f4be90e4087bc1dbbc28cd88a
Panteve <diegorojer@gmail.com>
2024-05-01 20:02:03 -0500

    cambio

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.set("port", process.env.PORT || 3000);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(compression());
-app.use(cors({ origin: "http://localhost:4200" }));
+app.use(cors());//{ origin: "http://localhost:4200" }
 
 //Conexión BD Atlas
 mongoose.connect(process.env.MONGODB_URI)
